8dfb8f39fd1ef2877bc24a362dd72947357a5774
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2017-04-04 21:16:11 -0600

    Mountain Car CTS

--- NEAT/evolve.py
+++ NEAT/evolve.py
@@ -8,10 +8,9 @@ import nes
 from neat.reporting import *
 import logging
 
+
 class OpenAITask:
     runs_per_net = 1
-    test_repeat = 10
-    success_threshold = None
     n_cpus = 8
 
     def __init__(self):
@@ -44,10 +43,21 @@ class OpenAITask:
         else:
             return fitness
 
+    def load_config(self):
+        local_dir = os.path.dirname(__file__)
+        config_path = os.path.join(local_dir, 'config-%s.txt' % self.tag)
+        config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                             neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                             config_path)
+        return config
+
+
 class CartPole(OpenAITask):
     gym_name = 'CartPole-v0'
     tag = 'cartpole'
-    step_limit = 600
+    step_limit = 200
+    test_repeat = 50
+    success_threshold = 195
     angle_limit_radians = 15 * math.pi / 180
     position_limit = 2.4
 
@@ -68,6 +78,10 @@ class CartPole(OpenAITask):
 
     def get_env(self):
         return gym.make(self.gym_name)
+
+    def set_step_limit(self, step_limit):
+        return
+
 
 class MountainCar(OpenAITask):
     gym_name = 'MountainCar-v0'
@@ -93,6 +107,48 @@ class MountainCar(OpenAITask):
         env = gym.make(self.gym_name)
         env._max_episode_steps = self.step_limit
         return env
+
+    def set_step_limit(self, step_limit):
+        self.step_limit = step_limit
+        self.env._max_episode_steps = step_limit
+
+
+class MountainCarCTS(OpenAITask):
+    gym_name = 'MountainCarContinuous-v0'
+    tag = 'mountain-car-cts'
+    step_limit = 250
+    test_repeat = 1
+    success_threshold = 90
+
+    def __init__(self):
+        OpenAITask.__init__(self)
+
+    def scale_state(self, state):
+        return [(state[0] + 1.2) / 1.8,
+                (state[1] + 0.07) / 0.14]
+
+    def get_action(self, value):
+        return np.tanh(value)
+
+    def get_fitness(self, reward):
+        return reward
+
+    def get_env(self):
+        env = gym.make(self.gym_name)
+        env._max_episode_steps = self.step_limit
+        return env
+
+    def set_step_limit(self, step_limit):
+        self.step_limit = step_limit
+        self.env._max_episode_steps = step_limit
+
+    def run(self):
+        config = self.load_config()
+        winner, _, all_fitnesses = evolve(config, 300, 250)
+        net = neat.nn.FeedForwardNetwork.create(winner, config)
+        while True:
+            self.play(net, True, False)
+
 
 class SuperMario:
     runs_per_net = 1
@@ -148,8 +204,10 @@ class SuperMario:
             state = self.scale_state(info['tiles'])
         return max_x
 
+
 # task = CartPole()
-task = MountainCar()
+# task = MountainCar()
+task = MountainCarCTS()
 # task = SuperMario()
 
 logger = logging.getLogger(__name__)
@@ -164,6 +222,7 @@ ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
+
 def eval_genome(genome, config):
     net = neat.nn.FeedForwardNetwork.create(genome, config)
     fitnesses = []
@@ -172,18 +231,27 @@ def eval_genome(genome, config):
         fitnesses.append(fitness)
     return min(fitnesses)
 
+
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
         genome.fitness = eval_genome(genome, config)
 
+
 class CustomReporter(BaseReporter):
+    def __init__(self):
+        self.all_fitnesses = []
+
     def found_solution(self, config, generation, best):
         self.generation = generation
+
+    def post_evaluate(self, config, population, species, best_genome):
+        fitnesses = [c.fitness for c in itervalues(population)]
+        self.all_fitnesses.append(fitnesses)
 
 
 def evolve(config, pop_size, step_limit):
     config.pop_size = pop_size
-    task.step_limit = step_limit
+    task.set_step_limit(step_limit)
 
     pop = neat.Population(config)
     stats = neat.StatisticsReporter()
@@ -195,26 +263,51 @@ def evolve(config, pop_size, step_limit):
     pe = neat.ParallelEvaluator(task.n_cpus, eval_genome)
     winner = pop.run(pe.evaluate)
 
-    return winner, reporter.generation
+    return winner, reporter.generation, reporter.all_fitnesses
 
-def run():
+
+def run_cartpole():
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-%s.txt' % task.tag)
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+    pop_sizes = [300, 250, 200, 150, 100]
+    step_limit = 200
+    runs = 30
+    results = dict()
+    for pop_size in pop_sizes:
+        success_generation = np.zeros(runs)
+        fitness_info = []
+        for r in range(runs):
+            logger.debug('pop size: %d, step limit: %d, run: %d' % (pop_size, step_limit, r))
+            winner, success_generation[r], all_fitnesses = evolve(config, pop_size, step_limit)
+            fitness_info.append(all_fitnesses)
+        results[(pop_size, step_limit)] = (success_generation, fitness_info, winner)
+        with open('statistics-%s.bin' % task.tag, 'wb') as f:
+            pickle.dump(results, f)
+
+
+def run_mountaincar():
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-%s.txt' % task.tag)
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_path)
 
-    pop_sizes = [250, 200, 150]
-    step_limits = [250, 200, 150]
+    pop_sizes = [300, 250, 200, 150]
+    step_limits = [300, 250, 200, 150]
     runs = 30
     results = dict()
     for pop_size in pop_sizes:
         for step_limit in step_limits:
             success_generation = np.zeros(runs)
+            fitness_info = []
             for r in range(runs):
                 logger.debug('pop size: %d, step limit: %d, run: %d' % (pop_size, step_limit, r))
-                winner, success_generation[r] = evolve(config, pop_size, step_limit)
-            results[(pop_size, step_limit)] = (success_generation, winner)
+                winner, success_generation[r], all_fitnesses = evolve(config, pop_size, step_limit)
+                fitness_info.append(all_fitnesses)
+            results[(pop_size, step_limit)] = (success_generation, fitness_info, winner)
             with open('statistics-%s.bin' % task.tag, 'wb') as f:
                 pickle.dump(results, f)
 
@@ -223,7 +316,51 @@ def run():
     with open('winner-%s.bin' % task.tag, 'wb') as f:
         pickle.dump(winner, f)
 
-    # print(winner)
+def draw_mountaincar():
+    with open('statistics-%s.bin' % task.tag, 'rb') as f:
+        results = pickle.load(f)
+    pop_sizes = [300, 250, 200, 150]
+    step_limits = [300, 250, 200, 150]
+    runs = 30
+    for pop_size in pop_sizes:
+        for step_limit in step_limits:
+            success_generation, all_fitnesses, _ = results[(pop_size, step_limit)]
+            success_generation += 1
+            success_fitness = np.zeros(runs)
+            for run, fitnesses in enumerate(all_fitnesses):
+                for generation_fitness in fitnesses:
+                    success_fitness[run] += np.sum(np.array(generation_fitness))
+            print 'pop size: %d, step limit: %d, avg generation: %f(%f), avg steps %f(%f)' % (
+                pop_size, step_limit,
+                np.mean(success_generation),
+                np.std(success_generation) / np.sqrt(runs),
+                -np.mean(success_fitness),
+                np.std(success_fitness) / np.sqrt(runs)
+            )
+
+
+def draw_cartpole():
+    with open('statistics-%s.bin' % task.tag, 'rb') as f:
+        results = pickle.load(f)
+    pop_sizes = [300, 250, 200, 150, 100]
+    step_limits = [200]
+    runs = 30
+    for pop_size in pop_sizes:
+        for step_limit in step_limits:
+            success_generation, all_fitnesses, _ = results[(pop_size, step_limit)]
+            success_generation += 1
+            success_fitness = np.zeros(runs)
+            for run, fitnesses in enumerate(all_fitnesses):
+                for generation_fitness in fitnesses:
+                    success_fitness[run] += np.sum(np.array(generation_fitness))
+            print 'pop size: %d, avg generation: %f(%f), avg steps %f(%f)' % (
+                pop_size,
+                np.mean(success_generation),
+                np.std(success_generation) / np.sqrt(runs),
+                np.mean(success_fitness),
+                np.std(success_fitness) / np.sqrt(runs)
+            )
+
 
 def show():
     local_dir = os.path.dirname(__file__)
@@ -234,15 +371,20 @@ def show():
 
     with open('statistics-%s.bin' % task.tag, 'rb') as f:
         results = pickle.load(f)
-    # print results
+        # print results
 
-    # winner = results[(250, 250)][1]
-    # with open('winner-%s.bin' % task.tag, 'rb') as f:
-    #     winner = pickle.load(f)
-    # print(winner)
-    # net = neat.nn.FeedForwardNetwork.create(winner, config)
-    # print task.play(net, True)
+        # winner = results[(250, 250)][1]
+        # with open('winner-%s.bin' % task.tag, 'rb') as f:
+        #     winner = pickle.load(f)
+        # print(winner)
+        # net = neat.nn.FeedForwardNetwork.create(winner, config)
+        # print task.play(net, True)
+
 
 if __name__ == '__main__':
     # show()
-    run()
+    # run_mountaincar()
+    # run_cartpole()
+    # draw_mountaincar()
+    # draw_cartpole()
+    task.run()
